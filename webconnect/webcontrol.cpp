6ddbf3dc2120b3ec6c64ada13261397ec17b455e
Jon Morgan <jonmmorgan@gmail.com>
2010-07-11 22:58:31 +1000

    Allowed a URL to be set without the web control grabbing the focus. Initial idea from http://www.kirix.com/forums/viewtopic.php?f=25&t=687.

--- webconnect/webcontrol.cpp
+++ webconnect/webcontrol.cpp
@@ -2893,7 +2893,8 @@ wxDOMDocument wxWebControl::GetDOMDocument()
 
 void wxWebControl::OpenURI(const wxString& uri,
                            unsigned int load_flags,
-                           wxWebPostData* post_data)
+                           wxWebPostData* post_data,
+                           bool grab_focus)
 {
     if (!IsOk())
         return;
@@ -2936,7 +2937,7 @@ void wxWebControl::OpenURI(const wxString& uri,
     freeUnichar(ns_uri);
                                             
     ns_smartptr<nsIWebBrowserFocus> focus = nsRequestInterface(m_ptrs->m_web_browser);
-    if (!focus)
+    if (!focus || !grab_focus)
         return;
 
     focus->Activate();
