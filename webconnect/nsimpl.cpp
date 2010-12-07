af584b2baa31b3e9d451d93afed5ec830b0cf696
Jon Morgan <jonmmorgan@gmail.com>
2010-12-07 23:38:22 +1100

    Removed unused variables, an unused function, and a warning.

--- webconnect/nsimpl.cpp
+++ webconnect/nsimpl.cpp
@@ -95,11 +95,6 @@ struct XPCOMFunctionTable
 
 static XPCOMFunctionTable funcs;
 
-static bool isLibraryOk()
-{
-    return (funcs.Init != 0) ? true : false;
-}
-
 typedef nsresult (PR_CALLBACK *GetFrozenFunctionsFunc)(XPCOMFunctionTable *func_table,
                                                        const char* path);
 
@@ -238,7 +233,7 @@ nsresult XPCOMGlueStartup(const char* xpcom_dll_path)
     size_t i, count = deplibs.GetCount();
     for (i = 0; i < count; ++i)
     {
-        void* handle = dlopen(deplibs.Item(i).mbc_str(), RTLD_GLOBAL | RTLD_LAZY);
+        (void *)dlopen(deplibs.Item(i).mbc_str(), RTLD_GLOBAL | RTLD_LAZY);
     }
     
     // now load the functions from libxpcom.so
--- webconnect/webcontrol.cpp
+++ webconnect/webcontrol.cpp
@@ -4051,8 +4051,6 @@ bool wxWebControl::ExecuteJSCode(const wxString& js_code, wxString &result)
     if (ctx.empty())
         return false;
 
-    void* obj = sgo->GetGlobalJSObject();
-    
     nsEmbedString str;
     wx2ns(js_code, str);
 
